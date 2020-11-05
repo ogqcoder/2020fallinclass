44ded808ff3fd9c79ddc648663c34319cfcb5d7d
ogqcoder <gartho77@gmail.com>
2020-11-05 18:17:33 -0500

    initial commit

---
+++ client/src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+
+<div class="level section">
+
+<div class="level-item">
+
+
+    <form>
+        <div class="field">
+        <p class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="Email">
+            <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password">
+            <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control">
+            <button class="button is-success" @click.prevent="login">
+            Login
+            </button>
+        </p>
+        </div>
+    </form>
+    </div>
+</div>
+</template>
+
+<script>
+import session from "@/models/session";
+
+export default {
+    methods: {
+        login(){
+            session.user = {
+                name: 'Rabbi Moshe Plotkin',
+                handle: 'jewpaltz',
+                profile: 'https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/120552651_10102367831029710_8523278423201553541_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=Jnr3FRfM4FIAX8F5rC9&_nc_ht=scontent-lga3-1.xx&oh=14a477b3f03186158eccf30fe3a03bda&oe=5FA552C2'
+            }
+            session.addNotification('Yay! You logged in', 'success')
+            this.$router.push('feed')
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
---
+++ server/models/users.js
@@ -0,0 +1,44 @@
+/* B"H
+
+*/
+const mysql = require('./mysql');
+
+//const data = [{ name: 'Moshe', age: 43}, { name: 'Biden', age: 78 }]
+
+async function getAll(){
+    //throw { status: 501, message: "This is a fake error" }
+    //await Promise.resolve()
+    console.log("Called Get All")
+    return await mysql.query(`SELECT * FROM Users`);
+}
+
+async function get(id){
+    const rows = await mysql.query(`SELECT * FROM Users WHERE id=?`, [id]);
+    if(!rows.length) throw { status: 404, message: "Sorry, there is no such user" };
+    return rows;
+}
+
+async function getTypes(){
+    return await mysql.query(`SELECT id, Name FROM Types WHERE Type_id = 2`);
+}
+
+async function add(FirstName, LastName, DOB, Password, User_Type){
+    const sql = 'INSERT INTO `Users` (`created_at`, `FirstName`, `LastName`, `DOB`, `Password`, `User_Type`) VALUES ? ;';
+    const params = [[new Date(), FirstName, LastName, new Date(DOB), Password, User_Type]];
+    return await mysql.query(sql, [params]);
+}
+
+async function update(id, FirstName, LastName, DOB, Password, User_Type){
+    const sql = 'UPDATE `Users` SET ? WHERE `id` = ?;';
+    const params = { created_at: new Date(), FirstName, LastName, DOB: new Date(DOB), Password, User_Type };
+    return await mysql.query(sql, [params, id]);
+}
+
+async function remove(id){
+    const sql = 'DELETE FROM `Users` WHERE `Users`.`id` = ?';
+    return await mysql.query(sql, [id]);
+}
+
+const search = async q => await mysql.query(`SELECT id, FirstName, LastName FROM Users WHERE LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`]);
+
+module.exports = { getAll, get, add, update, remove, getTypes, search }
